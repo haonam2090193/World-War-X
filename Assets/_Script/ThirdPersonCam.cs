f5e4124eabc81fbc0d8b685e30e4eb8cce4fa66e
haonam2090193 <129403060+haonam2090193@users.noreply.github.com>
2023-06-07 22:42:07 +0700

    [test] Combat Camera

--- Assets/_Script/ThirdPersonCam.cs
+++ Assets/_Script/ThirdPersonCam.cs
@@ -10,14 +10,31 @@ public class ThirdPersonCam : MonoBehaviour
     [SerializeField] private Rigidbody rb;
 
     [SerializeField] private float rotationSpeed;
-    
+    [SerializeField] private Transform combatLookAt;
+    public CameraStyle currentStyle;
+    public enum CameraStyle { Basic, Combat}
     void Update()
     {
         Vector3 viewDir = player.position - new Vector3(transform.position.x, player.position.y, transform.position.z);
         orientation.forward = viewDir.normalized;
 
-        float horizontalInput = Input.GetAxis("Horizontal");
-        float verticalInput = Input.GetAxis("Vertical");
-        Vector3 inputDir = orientation.forward *verticalInput + orientation.right * horizontalInput;
+        if (currentStyle == CameraStyle.Basic)
+        {
+            float horizontalInput = Input.GetAxis("Horizontal");
+            float verticalInput = Input.GetAxis("Vertical");
+            Vector3 inputDir = orientation.forward * verticalInput + orientation.right * horizontalInput;
+            if(inputDir != Vector3.zero)
+            {
+                playerObj.forward = Vector3.Slerp(playerObj.forward, inputDir.normalized, Time.deltaTime * rotationSpeed);
+            }
+        }
+        else if(currentStyle == CameraStyle.Combat)
+        {
+            Vector3 dirToCombatLookAt = combatLookAt.position - new Vector3(transform.position.x, combatLookAt.position.y, transform.position.z);
+            orientation.forward = dirToCombatLookAt.normalized;
+
+        }
+        
+        
     }
 }
